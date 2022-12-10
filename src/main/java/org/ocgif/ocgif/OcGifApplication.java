8216706acc3ac20b26a64b941b7361fa422280ba
othmed <med.othmani@outlook.fr>
2022-12-11 00:24:05 +0100

    init

---
+++ src/main/java/org/ocgif/ocgif/OcGifApplication.java
@@ -0,0 +1,13 @@
+package org.ocgif.ocgif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcGifApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcGifApplication.class, args);
+    }
+
+}
